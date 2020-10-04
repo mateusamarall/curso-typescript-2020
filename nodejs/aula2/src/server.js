f6aace761091e9840ae67f0199ae399aaf9e5d0b
mateusamarall <mateus.amaral018@gmail.com>
2020-10-04 17:32:25 -0300

    arquivos estaticos

--- nodejs/aula2/src/server.js
+++ nodejs/aula2/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+
+app.use(express.static('./public'));
+
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
