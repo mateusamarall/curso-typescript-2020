68822a33d0016b9056c3d782cb4fee56ef67b39d
mateusamarall <mateus.amaral018@gmail.com>
2020-10-12 12:46:04 -0300

    criand o projeto de agenda

--- nodejs/aula2/src/server.js
+++ nodejs/aula2/src/server.js
@@ -3,17 +3,36 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const routes = require('./routes');
 
 mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('db connected');
     app.emit('pronto')
 })
-    .catch(e => console.log(e));
+.catch(e => console.log(e));
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flashmsg = require('connect-flash');
+const routes = require('./routes');
+
 
 
 app.use(express.static('./public'));
 
+const sessionsOptions = session({
+    secret:'secreteste',
+    store: new MongoStore({mongooseConnection:mongoose.connection}),
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge:1000 * 60 * 60 * 24 * 7,
+        httpOnly:true
+
+    }
+});
+
+app.use(sessionsOptions);
+app.use(flashmsg());
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
