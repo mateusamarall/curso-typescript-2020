d1f4e6908cfa9e1ba1bf60f7d381bf126d3b6c43
mateus <mateus@mateus.local>
2020-11-09 23:36:45 -0300

    criado funcionalidade para mostrar erros

--- projeto-agenda/src/controllers/loginController.js
+++ projeto-agenda/src/controllers/loginController.js
@@ -8,11 +8,13 @@ module.exports ={
     const login = new Login(req.body);
 
     await login.register();
+    
     if(login.errors.length > 0){
       req.flash('errors', login.errors);
       req.session.save(function(){
         res.redirect('back');
       });
+      
     }
 
     res.send(login.user);
